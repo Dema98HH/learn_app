1b69babf4b845957602fb8295c7a00b24cf1a6d5
Dema98HH <w.demenkow@gmail.com>
2022-08-29 11:35:46 +0200

    cards with Summary button

--- frontend/src/views/Cart.vue
+++ frontend/src/views/Cart.vue
@@ -1,7 +1,10 @@
 <template>
 <div class="row justify-content-center">
+        <div class="col-10 justify-cotent-starts">
+            <h2 class="col-2 mt-2">Carts</h2>
+        </div>
     <div class="col-10">
-        <h2 class="mt-3">Carts</h2>
+        <!---->
     <div class="card mt-3">
         <div class="card-body">
             <!---->
@@ -27,6 +30,30 @@
             <!---->
         </div>
     </div>
+
+    <div class="col-10 mt-4">
+            <h2 class="col-2 mt-2">Summary</h2>
+    </div>
+
+    <div class="card mt-3">
+        <div class="card-body text-align-start">
+            <!---->
+            <div class="row mt-3">
+                <div class="col-3">
+                    <strong class="margin-left">${{ cartTotalPrice.toFixed(2) }}</strong>
+                </div>
+                <div class="col-2">
+                    <p>{{cartTotalLength}} Items </p>
+                </div>
+            </div>
+            <!---->
+        </div>
+    </div>
+
+    <div class="col-1 mt-4">
+        <router-link to="/cart/checkout" class="btn btn-success">Checkout</router-link>
+    </div>
+
 </div>
 </div>
 </template>
@@ -41,9 +68,8 @@ import CartItem from '@/components/CartItem.vue'
     export default {
         name: 'Cart',
         components: {
-    CartItem,
-    CartItem
-},
+            CartItem,
+        },
         data (){
             return {
                 cart: {
@@ -58,6 +84,11 @@ import CartItem from '@/components/CartItem.vue'
             cartTotalLength() {
                 return this.cart.items.reduce((acc, curVal) => {
                     return acc += curVal.quantity
+                }, 0)
+            },
+            cartTotalPrice() {
+                return this.cart.items.reduce((acc, curVal) => {
+                    return acc += curVal.product.price * curVal.quantity
                 }, 0)
             }
         }
